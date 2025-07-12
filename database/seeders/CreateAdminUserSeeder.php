e501258b4fd862a6fd38f916815d6306e34f4214
Ing-Vanly <ingvanly168@gmail.com>
2025-07-12 09:15:46 +0700

    Add Role as employee and user

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -16,7 +16,7 @@ class CreateAdminUserSeeder extends Seeder
     public function run(): void
     {
         $user = User::create([
-            'name' => 'ingvanly',
+            'name' => 'admin',
             'email' => 'admin@gmail.com',
             'password' => bcrypt('12345678')
         ]);
@@ -28,5 +28,24 @@ class CreateAdminUserSeeder extends Seeder
         $role->syncPermissions($permissions);
 
         $user->assignRole([$role->id]);
+        // ========== Employee ==========
+        $employee = User::create([
+            'name' => 'employee',
+            'email' => 'employee@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $employeeRole = Role::firstOrCreate(['name' => 'Employee']);
+        $employee->assignRole($employeeRole);
+
+        // ========== User ==========
+        $user = User::create([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $userRole = Role::firstOrCreate(['name' => 'User']);
+        $user->assignRole($userRole);
     }
 }
